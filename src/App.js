7b3a92a85a597b8ce0341d24c4478d53aba4b3e6
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-05-15 14:48:16 -0400

    learning how to use reactfire and update firestore collections

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React from "react";
-import { useFirestoreDocData, useFirestore } from "reactfire";
+
 import "firebase/auth";
 import "firebase/firestore";
 import "./App.css";
 
 import SignIn from "./components/SignIn";
-
-const Burrito = () => {
-	const burritoRef = useFirestore().collection("tryreactfire").doc("burrito");
-
-	const { status, data } = useFirestoreDocData(burritoRef);
-
-	if (status === "loading") {
-		return <p>Fetching burrito flavor...</p>;
-	}
-
-	return <p>The burrito is {data.yummy ? "good" : "bad"}!</p>;
-};
+import Burrito from "./components/Burrito";
 
 function App() {
 	return (
