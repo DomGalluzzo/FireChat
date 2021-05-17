972cc608f73efdafa6ec5973d3bb86f053baf2cd
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-05-17 17:54:55 -0400

    messages

--- src/components/Chatroom.js
+++ src/components/Chatroom.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
 	useFirestoreDocData,
 	useFirestore,
 	useFirestoreCollectionData,
 } from "reactfire";
 
-import { Button, Container, FormControl, InputGroup } from "react-bootstrap";
+import { Button, Container, Form } from "react-bootstrap";
 
 const Chatroom = () => {
+	const [text, setText] = useState("");
+	const [message, setMessage] = useState("");
+
+	useEffect(() => {}, [message]);
+
 	const chatroomRef = useFirestore()
 		.collection("chatrooms")
 		.doc("roomA")
 		.collection("messages");
 
 	const { status, data } = useFirestoreCollectionData(chatroomRef);
+
+	const getTextInput = (e) => {
+		setText(e.target.value);
+	};
+
+	const handleSendMessage = (e) => {
+		e.preventDefault();
+		setMessage(text);
+	};
 
 	if (status === "loading") {
 		return <p>loading...</p>;
@@ -30,12 +44,21 @@ const Chatroom = () => {
 				{data.map((message) => {
 					return <h5>{message.content}</h5>;
 				})}
-				<InputGroup>
-					<FormControl placeholder="Message..." aria-label="User's Message" />
-					<InputGroup.Append>
-						<Button variant="success">Send</Button>
-					</InputGroup.Append>
-				</InputGroup>
+				<Form inline className="message-box-form">
+					<Form.Group controlId="formMessage" className="message-box-text-area">
+						<Form.Control
+							placeholder="Message..."
+							aria-label="User's Message"
+							as="textarea"
+							rows={1}
+							value={text}
+							onChange={getTextInput}
+						/>
+					</Form.Group>
+					<Button variant="success" type="submit" onClick={handleSendMessage}>
+						Send
+					</Button>
+				</Form>
 			</Container>
 		</Container>
 	);
